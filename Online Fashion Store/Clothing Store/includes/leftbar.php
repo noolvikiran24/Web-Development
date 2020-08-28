2ffb4626b4a660ca39e11f453e5cbd8bb19a0905
noolvikiran24 <noolvikiran24@gmail.com>
2020-08-27 23:48:45 -0500

    Added readme.txt

--- Online Fashion Store/Clothing Store/includes/leftbar.php
+++ Online Fashion Store/Clothing Store/includes/leftbar.php
@@ -2,6 +2,5 @@
     <div class="col-md-2">
     	<div class="card bg-dark" style="border: 0px; margin-top: 4px;"><p class="text-light text-center">Shop By</p>
     	<div class="card" style="border: 0px;"><p class="text-center"><a href="index.php?maincategory=fashion" class="text-dark">Fashion</a></p></div>
-    	<div class="card"><p class="text-center"><a href="index.php?maincategory=electronics" class="text-dark">Electronics</a></p></div>
     	</div>
     </div>
