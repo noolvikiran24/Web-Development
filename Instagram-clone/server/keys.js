bfbee72db15420e2b73a7aec8bc6d37dc585a24c
noolvikiran24 <noolvikiran24@gmail.com>
2020-09-14 13:08:53 -0500

    Deleted

--- Instagram-clone/server/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    MONGOURI:"mongodb+srv://kirannoolvi:8Yd4WIvsRA7b5qOi@cluster0-kckm4.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "hsherehancksrheererscsftrtr"
-
-}
-
