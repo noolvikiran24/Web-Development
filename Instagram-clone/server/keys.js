21b44f9ce46fb0f01566031c420d8f6e83d09093
noolvikiran24 <noolvikiran24@gmail.com>
2020-09-14 13:19:28 -0500

    Added Instagram-clone

---
+++ Instagram-clone/server/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGOURI:"mongodb+srv://kirannoolvi:8Yd4WIvsRA7b5qOi@cluster0-kckm4.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET: "hsherehancksrheererscsftrtr"
+
+}
+
